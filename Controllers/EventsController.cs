c3a5a23ebe1e387113082cfab5611af9a466f42d
Usha R <a@b.com>
2020-10-29 18:03:01 +1100

    Initial Commit

---
+++ Controllers/EventsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FunFridayAPI.DataAccess;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FunFridayAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EventsController : ControllerBase
+    {
+        public readonly FunFridayDBContext funfridaydbContext;
+        public EventsController(FunFridayDBContext db)
+        {
+        funfridaydbContext = db;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var events = funfridaydbContext.Events.Select(e => e.Name);
+            return events.ToList();
+            //return new string[] { "Hello", "Hai" };
+        }
+     }
+}
---
+++ DataAccess/FunFridayDBContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FunFridayAPI.DataAccess
+{
+    public class FunFridayDBContext : DbContext
+    {
+        public FunFridayDBContext()
+        {
+
+        }
+
+        public FunFridayDBContext(DbContextOptions<FunFridayDBContext> dbContextOptions):base(dbContextOptions)
+        {
+            
+        }
+        public DbSet<Event> Events { get; set; }
+
+    }
+}
